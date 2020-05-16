a3ce61a85e57178fb9954c2f03c8dcbd9dc0bee9
Thiago Silva <thiago.silva@outlook.com>
2020-05-16 06:49:39 -0300

    Extension methods

---
+++ ExtensionMethods/Extensions/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿
+namespace System
+{
+    static class StringExtensions
+    {
+        public static string Cut(this string thisObj, int length)
+        {
+            if (thisObj.Length <= length)
+            {
+                return thisObj;
+            }
+            else
+            {
+                return thisObj.Substring(0, length) + "...";
+            }
+        }
+    }
+}
---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2020, 5, 16, 4, 40, 59);
+            Console.WriteLine(dt.ElapsedTime());
+
+            Console.WriteLine();
+            string s1 = "Bom dia queridos estudantes!";
+            Console.WriteLine(s1.Cut(10));
+        }
+    }
+}
